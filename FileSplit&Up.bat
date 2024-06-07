7ae215749f23fd07b1979139da819af80b3f0c4c
TanmoyTheBoT <88877195+TanmoyTheBoT@users.noreply.github.com>
2024-06-08 01:53:24 +0600

    Update FileSplit&Up.bat

--- FileSplit&Up.bat
+++ FileSplit&Up.bat
@@ -2,14 +2,14 @@
 title Execute Python Script
 
 :: Run the Python script
-python "D:\Tanmoy\Github\telegram-upload-upSplit\upSplit\FileSplit.py" %*
+python "c:\fullpath\upSplit\FileSplit.py" %*
 
 :: Check if the Python script completed successfully
 if %errorlevel% equ 0 (
     echo Python script completed successfully.
     echo Now executing the .bat file...
     pause
-    call "D:\Tanmoy\Github\telegram-upload-upSplit\upSplit\uploadALL.bat"
+    call "c:\fullpath\upSplit\upSplit\uploadALL.bat"
 ) else (
     echo Python script encountered an error.
     pause
